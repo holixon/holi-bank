962dec73dbef4c2f8a4c649bb378b1b490dc1e7d
Jan Galinski <jan.galinski@holisticon.de>
2018-07-10 09:20:29 +0200

    init with start.spring.io

---
+++ src/main/kotlin/de/holisticon/bank/holibank/HoliBankApplication.kt
@@ -0,0 +1,11 @@
+package de.holisticon.bank.holibank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HoliBankApplication
+
+fun main(args: Array<String>) {
+    runApplication<HoliBankApplication>(*args)
+}
---
+++ src/test/kotlin/de/holisticon/bank/holibank/HoliBankApplicationTests.kt
@@ -0,0 +1,16 @@
+package de.holisticon.bank.holibank
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class HoliBankApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

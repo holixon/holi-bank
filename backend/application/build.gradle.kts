c63f928fe30132b6785040aa6703459cf1144f60
Jan Galinski <jan.galinski@holisticon.de>
2018-07-23 02:28:31 +0200

    switch to managed version

--- backend/application/build.gradle.kts
+++ backend/application/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   kotlin("plugin.spring") version Versions.kotlin
   id("org.springframework.boot") version Versions.springBoot
-  id("io.spring.dependency-management") version "1.0.6.RELEASE"
+  id("io.spring.dependency-management") version Versions.springBootDependencyManagement
 }
 
 dependencies {

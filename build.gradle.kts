9bc4abd48787af5a76ea3c2a1606746bc5d63aa4
Jan Galinski <jan.galinski@holisticon.de>
2018-07-23 02:00:29 +0200

    works with buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,14 @@
-import de.holisticon.bank.build.Versions
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
 
+plugins {
     base
-    application
     idea
 
     kotlin("jvm") version Versions.kotlin
-    id("org.jetbrains.kotlin.plugin.spring") version Versions.kotlin
-
-    id("org.springframework.boot") version Versions.springBootVersion
-
-    //id("org.springframework.boot")
-    //id("io.spring.dependency-management")
-
-    id("io.spring.dependency-management") version "1.0.5.RELEASE"
+    kotlin ("plugin.spring") version Versions.kotlin
+    id("org.springframework.boot") version Versions.springBoot
+    id("io.spring.dependency-management") version Versions.springBootDependencyManagement
 }
 
 
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,12 +1,20 @@
-package de.holisticon.bank.build
+import org.gradle.kotlin.dsl.version
+import org.gradle.plugin.use.PluginDependenciesSpec
+import org.gradle.plugin.use.PluginDependencySpec
 
 object Versions {
   val kotlin = "1.2.51"
-  val springBootVersion = "2.0.3.RELEASE"
+  val springBoot = "2.0.3.RELEASE"
+  val springBootDependencyManagement = "1.0.6.RELEASE"
 }
 
-object Dependencies {
-
-
-
-}
+//
+//object Dependencies {
+//
+//  val springBootGradlePlugin  = "org.springframework.boot:spring-boot-gradle-plugin:${Versions.springBoot}"
+//  val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+//
+//  val kotlinJvm = "org.jetbrains.kotlin.jvm:${Versions.kotlin}"
+//  val kotlinSpring = "org.jetbrains.kotlin.plugin.spring:${Versions.kotlin}"
+//
+//}
